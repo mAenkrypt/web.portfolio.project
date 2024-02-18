8e9208c3afff9a9eb711c89261142a671cc08ab6
Massee Akbar <massee.akbar@Massees-MBP.attlocal.net>
2024-02-17 18:12:22 -0800

    coursework skeleton

---
+++ functions.js
@@ -0,0 +1,8 @@
+function toggle_visibility(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
