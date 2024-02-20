53b2c04609ecdd2c0403fb58be928c1b58c9d985
Massee Akbar <massee.akbar@Massees-MBP.attlocal.net>
2024-02-20 15:48:54 -0800

    links to sm and coursework

--- functions.js
+++ functions.js
@@ -6,3 +6,13 @@ function toggle_visibility(id) {
         content.style.display = "none";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.toggle-section').forEach(item => {
+        item.addEventListener('click', () => {
+            const contentId = item.getAttribute('data-target');
+            const content = document.getElementById(contentId);
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+});
